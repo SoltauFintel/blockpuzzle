9f5229a1450e83aa9d2d775222aeebd01839be86
Marcus Warm <warm@mwvb.de>
2020-08-30 20:00:56 +0200

    show phone top status bar

--- app/src/main/java/de/mwvb/blockpuzzle/MainActivity.kt
+++ app/src/main/java/de/mwvb/blockpuzzle/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
         spielfeld.setGame(game)
