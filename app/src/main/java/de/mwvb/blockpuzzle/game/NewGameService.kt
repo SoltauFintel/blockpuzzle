5c4424421fc91be39303051da8dc9b5f7b080de9
Marcus Warm <warm@mwvb.de>
2020-12-06 10:22:31 +0100

    Remove enemy from planet (backdoor)

--- app/src/main/java/de/mwvb/blockpuzzle/game/NewGameService.kt
+++ app/src/main/java/de/mwvb/blockpuzzle/game/NewGameService.kt
@@ -8,7 +8,16 @@ class NewGameService {
     fun newGame(per: IPersistence) {
         val planet = PlanetAccess(per).planet
         per.setGameID(planet)
-        per.saveScore(-1)
+        var score = per.loadScore()
+        if (score < 0 && per.loadMoves() == 0) {
+            score--
+            per.saveScore(score)
+            if (score > -9999 && score <= -3) {
+                per.clearOwner() // also clear enemy. It's for the case that the player thinks he has no chance to beat the enemy.
+            }
+        } else {
+            per.saveScore(-1)
+        }
         per.saveMoves(0)
         planet.isOwner = false
         per.savePlanet(planet)
